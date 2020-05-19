663c43624a088255b8e3b24a1862f32c6ce3f6c4
pchu-1207 <65568974+pchu-1207@users.noreply.github.com>
2020-05-19 14:05:28 -0600

    Update calcium_imaging_module_run.py

--- calcium_imaging_module_run.py
+++ calcium_imaging_module_run.py
@@ -1,23 +1,24 @@
-
+import os
 os.chdir("C:/Users/pchu1207/Google Drive/Data/Alexander/Code")
-import calcium_imaging_analysis_assemblies_synch as test
+#from calcium_imaging_analysis_assemblies_synch import analyze_ca
+import calcium_imaging_analysis_assemblies_synch as run
 #%%
-test = analyze_ca(file_directory= "//ucdenver.pvt/som/NEUROSURG/N_ LAB - Alexander/Data/calcium imaging/calcium imaging sample movie",
+test = run.analyze_ca(file_directory= "//ucdenver.pvt/som/NEUROSURG/N_ LAB - Alexander/Data/calcium imaging/calcium imaging sample movie",
                   shuffle_num= 100, #minimum 1000
                   scale_factor_time = 1, #pixel/micron value
                   scale_factor_spatial = 1, #time (sec) / frame
                   activity_data_file = 'deconv.csv', 
                   spatial_data_file = 'topo_centroids.csv', 
                   optimization_choice = 'no') #whether to optimize kmeans cluster more tightly or not 'no','yes'
-#%%
 test.load_data() 
 #%%
 test.deconv_freq() #get freq
+
 #%%
 test.synch_network_events()
 #%%
 test.network_contributions()
-
+    
 #%%
 test.kmeans_clustering()
 
@@ -44,10 +45,6 @@ test.summary_plots(t_sne_perplex = 30,
                    plot_3='true', 
                    plot_4='true')
 #%%
-
-
-
-
 
 
 
